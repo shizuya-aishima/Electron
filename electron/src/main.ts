71a3ce965947043289650a6212ffefe978671c29
aismszy <aismszy@gmail.com>
2023-09-13 13:08:08 +0000

    閉じても tray に入るように修正

--- electron/src/main.ts
+++ electron/src/main.ts
@@ -21,6 +21,8 @@ const showNotification = () => {
   }).show();
 };
 
+let isClose = false;
+
 const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 420,
@@ -34,23 +36,40 @@ const createWindow = () => {
   });
 
   mainWindow.loadFile('dist/index.html');
-  if (!app.isPackaged) {
-    mainWindow.webContents.openDevTools({ mode: 'detach' });
-  }
+  // if (!app.isPackaged) {
+  //   mainWindow.webContents.openDevTools({ mode: 'detach' });
+  // }
+
+  mainWindow.on('close', (event) => {
+    mainWindow.hide();
+    if (!isClose) {
+      event.preventDefault();
+    }
+  });
 
   return mainWindow;
 };
+// to handle quitting
+const handleQuit = () => {
+  isClose = true;
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+};
 
 app.whenReady().then(() => {
-  const win = createWindow();
+  const mainWindow = createWindow();
   const img = nativeImage.createFromPath(__dirname + '/../assets/favicon.png');
   let tray = new Tray(img);
   tray.setToolTip('Tray app');
 
   tray.setContextMenu(
-    Menu.buildFromTemplate([{ label: 'Quit', role: 'quit' }]),
+    Menu.buildFromTemplate([
+      { label: 'Quit', type: 'normal', click: handleQuit },
+    ]),
   );
-  reloadGlobalHotkeySettings(win);
+  tray.addListener('click', () => mainWindow.show());
+  reloadGlobalHotkeySettings(mainWindow);
 });
 
 const store = new Store();
@@ -70,10 +89,22 @@ ipcMain.handle(IPCKeys.LOAD, (event) => {
 
   return { top, lower };
 });
-// app.once('window-all-closed', () => app.quit());
 
 const reloadGlobalHotkeySettings = (win: BrowserWindow) => {
   globalShortcut.register('CommandOrControl+b', () => {
     win.webContents.send(IPCKeys.RECEIVE_MESSAGE, 'test message');
   });
 };
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', (e: Event) => {});
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
